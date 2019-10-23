fdb403edccbfb691c9245e850de8ecb78604a69b
Łukasz Kostrzewa <lukko98@gmail,com>
2019-10-23 20:15:49 +0200

    Created notification to show up automaticaly daily for a certain hour.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -13,6 +14,8 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.Calendar;
+
 import static com.example.myapplication.R.drawable.tul_logo;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,12 +24,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(Calendar.HOUR_OF_DAY, 6);
+        calendar.set(Calendar.MINUTE, 14);
+        calendar.set(Calendar.SECOND, 0);
+
+        Intent intent1 = new Intent(getApplicationContext(), AlarmReceiver.class);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 100, intent1, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+        am.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+
     }
 
     public void onClick(View view){
+        ////TO BYLO WCZESNIEJ
 
-        NotificationManager mNotificationManager =
+        /*NotificationManager mNotificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel("CHANNEL_ID",
@@ -34,11 +54,9 @@ public class MainActivity extends AppCompatActivity {
                     NotificationManager.IMPORTANCE_DEFAULT);
             //channel.setDescription("YOUR_NOTIFICATION_CHANNEL_DISCRIPTION");
             mNotificationManager.createNotificationChannel(channel);
-        }
+        }/*
 
-
-
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), "CHANNEL_ID")
+        /*NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), "CHANNEL_ID")
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("Daily TUL")
                 .setContentText("Politechnika jest super")
@@ -51,6 +69,6 @@ public class MainActivity extends AppCompatActivity {
         PendingIntent pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         mBuilder.setContentIntent(pi);*/
 
-        mNotificationManager.notify(0, mBuilder.build());
+        /*mNotificationManager.notify(0, mBuilder.build());*/
     }
 }
